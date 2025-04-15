8170467edd2bcc9f3b78bf69f89b2a0999dc1db4
Nitish <nitishkrsahu123@gmail.com>
2025-04-16 00:14:46 +0530

    DOM access using ID

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function refreshPage(){
+    document.getElementById("photo").src = "../src/3.png";
+    document.getElementById("registerBtn").value = "Register";
+    document.getElementById("loginBtn").value = "Login";
+}
